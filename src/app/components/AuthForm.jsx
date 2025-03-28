5103e301c3dcd850bd60035ec8766548aecea2ab
bk <josiasboco@gmail.com>
2025-03-28 13:30:39 +0100

    coorection bug import supabase

--- src/app/components/AuthForm.jsx
+++ src/app/components/AuthForm.jsx
@@ -1,5 +1,7 @@
 'use client'
-import { useState } from "react"
+import { useState } from "react";
+import { supabase } from "../utils/supabaseClient.js";
+
 export default function AuthForm() {
     const [isNewUser, setIsNewUser] = useState(false)
     const [email, setEmail] = useState('')
