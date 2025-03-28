bb2e8c030da9bc0601ae0a2dcf12879d57b7685e
bk <josiasboco@gmail.com>
2025-03-28 13:37:00 +0100

    next au lieu de nect

--- src/app/utils/supabaseClient.js
+++ src/app/utils/supabaseClient.js
@@ -3,7 +3,7 @@
 import { createBrowserClient } from "@supabase/ssr";
 
 const supabase = createBrowserClient(
-    process.env.NECT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 )
 
