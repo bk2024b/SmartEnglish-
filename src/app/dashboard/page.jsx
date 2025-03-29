8d4e9e5692c4114a34c13b516501d8b17b6f0980
bk <josiasboco@gmail.com>
2025-03-30 00:21:30 +0100

    use client

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React, { useState } from 'react';
 import SignOutButton from "../components/SignOutButton";
 import ActivitiesPopup from "../components/ActivitiesPopup"; // Importation du composant popup
--- src/app/dashboard/tasks/page.jsx
+++ src/app/dashboard/tasks/page.jsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React, { useState } from 'react';
 import SignOutButton from "../components/SignOutButton";
 
