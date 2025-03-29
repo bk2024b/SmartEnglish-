4e9e9a4a7e29cded966958f305ef9438620de70e
bk <josiasboco@gmail.com>
2025-03-29 01:26:58 +0100

    middleware added

---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,14 @@
+export default function Dashboard() {
+    return (
+        <main className="min-h-screen bg-gray-800 text-white relative p-10">
+            <div>
+                <div>
+                    <h1>Dashboard Apprenant</h1>
+                </div>
+            </div>
+            <div>
+                {/* SignOutButton */}
+            </div>
+        </main>
+    )
+}
