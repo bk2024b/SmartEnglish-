302ee1ff59ba36cc285d26cb17182a8d4374dd77
bk <josiasboco@gmail.com>
2025-03-29 10:47:53 +0100

    J'avais éffacé layout.js par inadvertance donc maintenant je l'ai remis

---
+++ src/app/layout.js
@@ -0,0 +1,33 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "SmartEnglish +",
+  description: "Six Mois de Coaching Premium en Anglais",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <Analytics />
+      <SpeedInsights />
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
+ 
