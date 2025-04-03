cf07673592c53d035a9e3417fbaf52256f9eeb87
bk <josiasboco@gmail.com>
2025-04-03 03:17:46 +0100

    admin dashboard implémenté

--- src/app/dashboard/admin/page.jsx
+++ src/app/dashboard/admin/page.jsx
@@ -280,7 +280,7 @@ export default function AdminDashboard  () {
         <div className="flex items-end">
           <button 
             className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700"
-            onClick={() => setOpenActivityForm(true)}
+            onClick={() => setActivityFormOpen(true)}
           >
             Ajouter une activité
           </button>
