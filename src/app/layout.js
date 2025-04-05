0b44956045887831b3aae8ba44660e81b89da88a
bk <josiasboco@gmail.com>
2025-04-05 12:35:06 +0100

    layout mis à jour

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,9 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import Head from "next/head";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -20,14 +22,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Analytics />
-      <SpeedInsights />
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <Head>
+        <link rel="apple-touch-icon" sizes="512x512" href="/icons/apple-touch-icon.png" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="description" content="Six Mois de Coaching Premium en Anglais" />
+        <title>SmartEnglish +</title>
+      </Head>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Analytics />
+        <SpeedInsights />
         {children}
       </body>
     </html>
   );
 }
- 
